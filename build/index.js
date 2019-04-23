847b72311455c131d1b3326cc0f3ae533fb10eae
Rafael Melo <rsmelo_@hotmail.com>
2019-04-23 00:09:49 -0300

    Changes to deploy to heroku

--- build/index.js
+++ build/index.js
@@ -10,44 +10,15 @@ var _expressGraphql = _interopRequireDefault(require("express-graphql"));
 
 var _pt_BR = _interopRequireDefault(require("faker/locale/pt_BR"));
 
-var _serverlessHttp = _interopRequireDefault(require("serverless-http"));
-
 var _schema = _interopRequireDefault(require("./schema"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])();
-
-var router = _express["default"].Router();
-
 app.use(_bodyParser["default"].json());
 app.get("/", function (req, res) {
-  res.sendFile("redirect.html", {
-    root: __dirname
-  });
+  res.sendFile(_path["default"].join(__dirname + "/redirect.html"));
 });
-router.get("/graphql", (0, _expressGraphql["default"])({
-  schema: _schema["default"],
-  rootValue: {
-    user: {
-      name: _pt_BR["default"].name.firstName(),
-      invoice: _pt_BR["default"].finance.amount(100, 1000, 2),
-      creditCardLimit: 3000.0,
-      lastPurchase: {
-        company: _pt_BR["default"].company.companyName(),
-        value: _pt_BR["default"].finance.amount(100, 500, 2)
-      },
-      balance: _pt_BR["default"].finance.amount(10000, 20000, 2),
-      rewardsPoints: _pt_BR["default"].finance.amount(20000, 30000, 2),
-      lastAcumulatedPoints: _pt_BR["default"].finance.amount(2000, 3000, 2),
-      suggestionRewards: {
-        value: _pt_BR["default"].finance.amount(200, 300, 2),
-        points: _pt_BR["default"].finance.amount(2000, 3000, 2)
-      }
-    }
-  },
-  graphiql: true
-}));
 app.use("/graphql", (0, _expressGraphql["default"])({
   schema: _schema["default"],
   rootValue: {
@@ -70,6 +41,4 @@ app.use("/graphql", (0, _expressGraphql["default"])({
   },
   graphiql: true
 }));
-app.use("/.netlify/functions/server", router);
-app.listen(3000);
-exports.handler = (0, _serverlessHttp["default"])(app);
+app.listen(5000);
--- server/index.js
+++ server/index.js
@@ -3,43 +3,15 @@ import path from "path";
 import bodyParser from "body-parser";
 import graphqlHttp from "express-graphql";
 import faker from "faker/locale/pt_BR";
-import serverless from "serverless-http";
 import schema from "./schema";
 
 const app = express();
-const router = express.Router();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.sendFile("redirect.html", { root: __dirname });
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/redirect.html"));
 });
-
-router.get(
-  "/graphql",
-  graphqlHttp({
-    schema,
-    rootValue: {
-      user: {
-        name: faker.name.firstName(),
-        invoice: faker.finance.amount(100, 1000, 2),
-        creditCardLimit: 3000.0,
-        lastPurchase: {
-          company: faker.company.companyName(),
-          value: faker.finance.amount(100, 500, 2)
-        },
-        balance: faker.finance.amount(10000, 20000, 2),
-        rewardsPoints: faker.finance.amount(20000, 30000, 2),
-        lastAcumulatedPoints: faker.finance.amount(2000, 3000, 2),
-        suggestionRewards: {
-          value: faker.finance.amount(200, 300, 2),
-          points: faker.finance.amount(2000, 3000, 2)
-        }
-      }
-    },
-    graphiql: true
-  })
-);
 
 app.use(
   "/graphql",
@@ -67,8 +39,4 @@ app.use(
   })
 );
 
-app.use("/.netlify/functions/server", router);
-
-app.listen(3000);
-
-exports.handler = serverless(app);
+app.listen(5000);
